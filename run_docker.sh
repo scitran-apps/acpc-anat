623c416f1311fdbcc5c123a102665248bab8165d
Michael Perry <lmperry@stanford.edu>
2018-10-03 10:58:05 -0700

    various updates/fixes

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@ docker run --rm -it \
 		-v /black/localhome/glerma/soft/acpc-anat/config_parsed.json:/flywheel/v0/config_parsed.json \
         -v /black/localhome/glerma/TESTDATA/acpcAnat/input:/flywheel/v0/input \
         -v /black/localhome/glerma/TESTDATA/acpcAnat/output:/flywheel/v0/output \
-        scitran/acpc-anat:1.0.0
+        vistalab/acpc-anat:1.0.0
